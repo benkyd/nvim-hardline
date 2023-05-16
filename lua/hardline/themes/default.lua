5f6e47a72d30ab2c58910cd17c8de1823da4683c
Benjamin Kyd <benjaminkyd@gmail.com>
2023-05-16 21:38:52 +0100

    Update default.lua

--- lua/hardline/themes/default.lua
+++ lua/hardline/themes/default.lua
@@ -1,15 +1,15 @@
 local colors = {
-  black = {gui = '#282C34', cterm = '235', cterm16 = '0'},
-  blue = {gui = '#61AFEF', cterm = '39', cterm16 = '4'},
-  cyan = { gui = '#56B6C2', cterm = '38', cterm16 = '6'},
-  green = {gui = '#98C379', cterm = '114', cterm16 = '2'},
-  grey_comment = {gui = '#5C6370', cterm = '59', cterm16 = '15'},
-  grey_cursor = {gui = '#2C323C', cterm = '236', cterm16 = '8'},
-  grey_menu = {gui = '#3E4452', cterm = '237', cterm16 = '8'},
+  black = {gui = '#181926', cterm = '235', cterm16 = '0'},
+  blue = {gui = '#8aadf4', cterm = '39', cterm16 = '4'},
+  cyan = { gui = '#91d7e3', cterm = '38', cterm16 = '6'},
+  green = {gui = '#a6da95', cterm = '114', cterm16 = '2'},
+  grey_comment = {gui = '#363a4f', cterm = '59', cterm16 = '15'},
+  grey_cursor = {gui = '#494d64', cterm = '236', cterm16 = '8'},
+  grey_menu = {gui = '#5b6078', cterm = '237', cterm16 = '8'},
   purple = {gui = '#F5BDE6', cterm = '170', cterm16 = '5'},
-  red = {gui = '#E06C75', cterm = '204', cterm16 = '1'},
-  white = {gui = '#ABB2BF', cterm = '145', cterm16 = '7'},
-  yellow = {gui = '#E5C07B', cterm = '180', cterm16 = '3'},
+  red = {gui = '#ed8796', cterm = '204', cterm16 = '1'},
+  white = {gui = '#cad3f5', cterm = '145', cterm16 = '7'},
+  yellow = {gui = '#eed49f', cterm = '180', cterm16 = '3'},
 }
 
 local inactive = {
